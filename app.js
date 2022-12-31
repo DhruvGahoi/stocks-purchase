be030f47d43c35c8c6bd7ba17cfb1cf7fbbd2e61
DhruvGahoi <dhruvgahoi11111@gmail.com>
2022-12-31 16:32:17 +0530

    showing the output
    finalising the code..and just showing the output

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 var initialPrice = document.querySelector('#initial-price');
 var stocksQty = document.querySelector('#stocks-quantity');
-var finalPrice = document.querySelector('#final-price');
+var currentPrice = document.querySelector('#current-price');
 var submitBtn = document.querySelector('#submit-btn');
 var outputBox = document.querySelector('#output-box');
+
+submitBtn.addEventListener('click',submitHandler);
+
+function submitHandler() {
+    var ip = Number(initialPrice.value);
+    var qty = Number(stocksQty.value);
+    var fp = Number(currentPrice.value);
+
+    calculateProfitAndLoss(ip,qty,fp);
+}
 
 function calculateProfitAndLoss(initial,quantity,current) {
     if (initial>current) {
         var loss = (initial - current)*quantity;
         var lossPercentage = (loss/initial)*100;
 
-        console.log('Oops!! The loss is', loss ,'and the percent is ',lossPercentage,'%');
+        showOutput('Oops!! The loss is' ,loss, ' and the percent is ',lossPercentage,'%');
     }
     else if (current>initial) {
         var profit  = (current - initial)*quantity;
         var profitPercentage = (profit/initial)*100;
         
-        console.log('Whoopsieee!! The profit is ',profit, 'and the percent is ',profitPercentage, '%');
+        showOutput('Whoopsieee!! The profit is ',profit, 'and the percent is ',profitPercentage, '%');
     }
     else {
-        console.log ('No pain No gain, No gain No pain');
+        showOutput ('No pain No gain, No gain No pain');
     }
 }
+function showOutput(message) {
+    outputBox.innerHTML = message;
+
+}
  
-calculateProfitAndLoss(10,10,10);
